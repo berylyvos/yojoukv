a9b8387bf9ee6f2c4daeb4e87e593fb6cba17e3f
Shingo Gnix <870656738@qq.com>
2024-03-07 15:37:23 +0800

    fix: use capital-case

--- src/kvraft/state_machine.go
+++ src/kvraft/state_machine.go
@@ -1,28 +1,28 @@
 package kvraft
 
 type InMemSM struct {
-	table map[string]string
+	Table map[string]string
 }
 
 func NewInMemSM() *InMemSM {
 	return &InMemSM{
-		table: make(map[string]string),
+		Table: make(map[string]string),
 	}
 }
 
 func (sm *InMemSM) Get(key string) (string, Err) {
-	if val, ok := sm.table[key]; ok {
+	if val, ok := sm.Table[key]; ok {
 		return val, OK
 	}
 	return "", ErrNoKey
 }
 
 func (sm *InMemSM) Put(key, value string) Err {
-	sm.table[key] = value
+	sm.Table[key] = value
 	return OK
 }
 
 func (sm *InMemSM) Append(key, value string) Err {
-	sm.table[key] += value
+	sm.Table[key] += value
 	return OK
 }
